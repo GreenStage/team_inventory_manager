df9269d81e6e2220b4ed7f9317bc7080caf4511e
Eduardo Gomes <eduardo.l.gomes@tecnico.ulisboa.pt>
2019-12-30 00:24:48 +0000

    updated

--- inventory_service/src/index.js
+++ inventory_service/src/index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = 80
+const port = process.env.PORT
 
 //CERTBOT
 app.get('/.well-known/acme-challenge/NUbwnLYxJxyMYFoXzVbGM_cR1RKIgTanhnZQMgf9pfY', function(req, res) {
