ef599e7f6014033a3c1dce8f1c5eb5bcc8068f3b
Eduardo Gomes <eduardo.l.gomes@tecnico.ulisboa.pt>
2019-12-30 00:44:33 +0000

    updated

---
+++ inventory_service/public/precache-manifest.62904e45fa178db0860cdcbdb2afd396.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e023e6dc4dc8a28b4e5b569f8ba539bf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0aa35c16736f9955d413",
+    "url": "/static/css/main.65bd437f.chunk.css"
+  },
+  {
+    "revision": "7f91b1a6badcb8c6b9b9",
+    "url": "/static/js/2.71401400.chunk.js"
+  },
+  {
+    "revision": "764ee7d7b2b37ea6ad2fc352f6f4060b",
+    "url": "/static/js/2.71401400.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0aa35c16736f9955d413",
+    "url": "/static/js/main.b4725c38.chunk.js"
+  },
+  {
+    "revision": "9286be7b84779780a902",
+    "url": "/static/js/runtime-main.38d7cc72.js"
+  }
+]);
